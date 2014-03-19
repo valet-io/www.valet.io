7bc6a1c55a90855c3b5d10afb98b5431847c606b
Ben Drucker <bvdrucker@gmail.com>
2014-03-19 15:30:14 -0400

    Development server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 'use strict';
 
-var gulp    = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var plugins    = require('gulp-load-plugins')();
+var nodeStatic = require('node-static');
+var http       = require('http');
+
+plugins.grunt(gulp);
 
 gulp.task('styles', function () {
   return gulp.src('styles/main.styl')
@@ -10,4 +15,26 @@ gulp.task('styles', function () {
       import: ['nib']
     }))
     .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('server', function(done) {
+  var server = new nodeStatic.Server('./build');
+  http.createServer(function (request, response) {
+    request.addListener('end', function () {
+      server.serve(request, response);
+    }).resume();
+  }).listen(8000, function() {
+    gutil.log('Server listening on port: ' + gutil.colors.magenta(8000));
+    done();
+  });
+});
+
+gulp.task('watch', ['server'], function () {
+  var livereload = plugins.livereload();
+  gulp.watch('styles/*.styl', ['styles']);
+  gulp.watch(['layouts/*', 'pages/**/*', 'partials/*'], ['grunt-assemble']);
+
+  gulp.watch('build/**/*').on('change', function (file) {
+    livereload.changed(file.path);
+  });
 });

f414bd5e63ae2f3599064bd3d25bb724a54df440
Ben Drucker <bvdrucker@gmail.com>
2014-03-19 14:49:58 -0400

    CSS build process

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+  return gulp.src('styles/main.styl')
+    .pipe(plugins.stylus({
+      use: ['nib'],
+      set: ['compress'],
+      import: ['nib']
+    }))
+    .pipe(gulp.dest('./build/css'));
+});

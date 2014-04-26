c08d41339a0a0fc058a2e1110e27f287cc837876
Ben Drucker <bvdrucker@gmail.com>
2014-04-26 18:37:56 -0400

    return stream from gulp images

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('images', function () {
-  gulp.src(['images/**/*'])
+  return gulp.src(['images/**/*'])
     .pipe(gulp.dest('build/images'));
 });
 

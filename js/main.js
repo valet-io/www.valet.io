663b6921390512f35b2435aea6040d8da1326b20
Ben Drucker <bvdrucker@gmail.com>
2014-06-30 11:18:45 -0400

    add mobile support to lead form and post to server

--- js/main.js
+++ js/main.js
@@ -28,9 +28,13 @@ $form.on('submit', function (event) {
   for (var field in $fields) {
     data[field] = $fields[field].val();
   }
-  $.post('http://site-api.valet.io', data)
+  $.post('http://site-api.valet.io/leads', data)
     .done(function () {
       $form.append('Thanks! We received your request and will be in touch soon.');
+      setTimeout(function () {
+        $form.hide();
+        $modal.hide();
+      }, 500);
     })
     .fail(function () {
       $form.append('Oh no! We were not able to submit your request. You can try again or <a href="mailto:jordan@valet.io">email us directly</a>.');
